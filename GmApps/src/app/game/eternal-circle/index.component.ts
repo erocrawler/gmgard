57bdc3dda0f6ad0871e216ad7331f7e1d7bfa66a
erocrawler <yiduo208@outlook.com>
2023-01-20 03:11:29 -0800

    remove obsolete scenario.ts file

--- GmApps/src/app/game/eternal-circle/index.component.ts
+++ GmApps/src/app/game/eternal-circle/index.component.ts
@@ -5,7 +5,6 @@ import { switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { EternalCircleGameScenario } from './eternal-circle-game-scenario.service';
 import { Dialog, GameScenario, GameStatus, Narrator } from '../../models/GameScenario';
-import { Scenarios } from './scenario';
 import { GameScenarios } from '../../shared/adv-game/scenario';
 
 @Component({
@@ -15,29 +14,6 @@ import { GameScenarios } from '../../shared/adv-game/scenario';
 export class IndexComponent implements OnInit {
 
   constructor(private gameSceneService: EternalCircleGameScenario, public dialog: MatDialog) {
-    this.seedScene = Scenarios.map(s => {
-      let gs: GameScenario = {
-        id: s.title,
-        dialogs: s.dialogs.map(d => {
-          let ds: Dialog = {
-            bgImg: d.bgImg,
-            effect: d.effect,
-            texts: d.texts
-          };
-          return ds;
-        }),
-        narrators: s.narrators.map(n => {
-          let ns: Narrator = {
-            avatar: n.avatar,
-            name: n.name,
-            display: n.display || n.name,
-          }
-          return ns;
-        }),
-        next: s.next || [],
-      };
-      return gs;
-    });
     this.scenarios = new GameScenarios(gameSceneService);
   }
 
@@ -45,7 +21,6 @@ export class IndexComponent implements OnInit {
   loading = true
   status: GameStatus
   playing = false;
-  seedScene: GameScenario[]
   isNewGame = true;
 
   ngOnInit() {
